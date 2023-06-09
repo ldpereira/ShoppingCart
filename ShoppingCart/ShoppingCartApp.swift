512b4a33995460172974cfc365c6960aa09c1f6d
Lucas Dalcol Pereira <lucas.pereira_1@philips.com>
2023-06-10 00:50:34 +0530

    Initial Commit

---
+++ ShoppingCart/ShoppingCartApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingCartApp.swift
+//  ShoppingCart
+//
+//  Created by Lucas Pereira on 10/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingCartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
